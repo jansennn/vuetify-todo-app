f9cf706bd898b27491d984da98bb1ab2d804570d
Andreas Tampubolon <andreasjr61@gmail.com>
2021-01-22 20:46:29 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about pa-6">
+    <h1>About verify todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto autem architecto eveniet sunt! Porro assumenda culpa non facilis dolores esse dignissimos, dolor suscipit! Impedit necessitatibus rerum quae illum quaerat mollitia!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error mollitia odio iste quasi minus fugiat quis non nulla numquam aut, saepe, sapiente suscipit debitis culpa officia. Molestiae, quam quia? Exercitationem.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea atque eum animi. Eaque non repudiandae aut delectus voluptas? Iure distinctio possimus deleniti quam dolores nostrum corrupti obcaecati sapiente pariatur numquam?
+    </p>
+  </div>
+</template>
